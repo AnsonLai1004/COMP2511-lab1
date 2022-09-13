ef612ad2dc4f31c7cb33742486dd73c861d11133
Anson Lai <z5361987@ad.unsw.edu.au>
2022-09-13 11:30:36 +1000

    lab02

--- src/splitter/Splitter.java
+++ src/splitter/Splitter.java
@@ -1,9 +1,15 @@
 package splitter;
-
+import java.util.Scanner;
 public class Splitter {
 
     public static void main(String[] args) {
         System.out.println("Enter a message: ");
         // Add your code
+        Scanner scanner = new Scanner(System.in);
+        String[] str = scanner.nextLine().split(" ");
+        for (String i : str) {
+            System.out.println(i);
+        }
+        scanner.close();
     }
 }
