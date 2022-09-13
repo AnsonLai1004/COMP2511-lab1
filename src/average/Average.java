aa298d12a42068cb9276a7b2d625dc4b34220ad7
Anson Lai <z5361987@ad.unsw.edu.au>
2022-09-13 11:17:11 +1000

    lab01

--- src/average/Average.java
+++ src/average/Average.java
@@ -7,13 +7,27 @@ public class Average {
      * @param the array of integer numbers
      * @return the average of the numbers
      */
-    public float computeAverage(int[] nums) {
+    public static float computeAverage(int[] nums) {
         float result = 0;
         // Add your code
+        float sum = 0;
+        for (int i : nums) {
+            sum += i;
+        }
+        //System.out.println(sum);
+
+        //System.out.println(nums.length);
+        result = sum / nums.length; 
+        //System.out.println(result);
         return result;
     }
 
     public static void main(String[] args) {
         // Add your code
+        int[] arr = {1, 2, 3, 4, 5, 6};
+        //Average ca = new Average();
+        //float ca2 = ca.computeAverage(arr);
+
+        System.out.println("The average is " + computeAverage(arr));
     }
 }
