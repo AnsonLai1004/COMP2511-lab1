64d739b8a619880353590f0bc550486998bf3a19
Anson Lai <z5361987@ad.unsw.edu.au>
2022-09-13 11:34:37 +1000

    lab02

--- src/splitter/Splitter.java
+++ src/splitter/Splitter.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 public class Splitter {
 
     public static void main(String[] args) {
-        System.out.println("Enter a message: ");
+        System.out.println("Enter a sentence specified by spaces only: ");
         // Add your code
         Scanner scanner = new Scanner(System.in);
         String[] str = scanner.nextLine().split(" ");
