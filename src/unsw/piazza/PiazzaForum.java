09c3ac4a69cc9142e05050ad4c9dc14314deaeb7
Anson Lai <z5361987@ad.unsw.edu.au>
2022-09-14 19:28:02 +1000

    piazza

--- src/unsw/piazza/PiazzaForum.java
+++ src/unsw/piazza/PiazzaForum.java
@@ -1,5 +1,6 @@
 package unsw.piazza;
 
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -8,30 +9,36 @@ import java.util.List;
  * @author Your Name
  */
 public class PiazzaForum {
-    
+    private String groupName;
+    private List<Thread> threads;
     /**
      * Initialises the new PiazzaForum with the given group name
      */
-    public PiazzaForum(String className) {}
+    public PiazzaForum(String className) {
+        this.groupName = className;
+        this.threads = new ArrayList<Thread>();
+    }
 
     /**
      * @return The name of the forum
      */
     public String getName() {
-        return null;
+        return groupName;
     }
 
     /**
      * Sets the name of the group of the Forum
      * @param name
      */
-    public void setName(String name) {}
+    public void setName(String name) {
+        this.groupName = name;
+    }
 
     /**
      * Returns a list of Threads in the Forum, in the order that they were published
      */
     public List<Thread> getThreads() {
-        return null;
+        return threads;
     }
 
     /**
@@ -43,7 +50,9 @@ public class PiazzaForum {
      * @param content
      */
     public Thread publish(String title, String content) {
-        return null;
+        Thread thread = new Thread(title, content);
+        this.threads.add(thread);
+        return thread;
     }
 
     /**
@@ -53,7 +62,17 @@ public class PiazzaForum {
      * @return
      */
     public List<Thread> searchByTag(String tag) {
-        return null;
+        List<Thread> result = new ArrayList<Thread>();
+        for (Thread thr : this.threads) {
+            List<String> tags = thr.getTags();
+            for (String str : tags) {
+                if (str == tag) {
+                    result.add(thr);
+                    break;
+                } 
+            }
+        }
+        return result;
     }
 
 }
--- src/unsw/piazza/Thread.java
+++ src/unsw/piazza/Thread.java
@@ -1,50 +1,66 @@
 package unsw.piazza;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 /**
  * A thread in the Piazza forum.
  */
 public class Thread {
-
+    private String title;
+    private List<String> posts;
+    private List<String> tags;
+    
     /**
      * Creates a new thread with a title and an initial first post.
      * @param title
      * @param firstPost
      */
-    public Thread(String title, String firstPost) {}
+    public Thread(String title, String firstPost) {
+        this.title = title;
+        this.posts.add(firstPost);
+        this.tags = new ArrayList<String>();
+    }
 
     /**
      * @return The title of the thread
      */
     public String getTitle() {
-        return null;
+        return title;
     }
 
     /**
      * @return A SORTED list of tags
      */
     public List<String> getTags() {
-        return null;
+        Collections.sort(tags);
+        return tags;
     }
 
     /**
      * @return A list of posts in this thread, in the order that they were published
      */
     public List<String> getPosts() {
-        return null;
+        return posts;
     }
 
     /**
      * Adds the given post object into the list of posts in the thread.
      * @param post
      */
-    public void publishPost(String post) {}
+    public void publishPost(String post) {
+        this.posts.add(post);
+    }
 
     /**
      * Allows the given user to replace the thread tags (list of strings)
      * @param tags
      */
-    public void setTags(String[] tags) {}
+    public void setTags(String[] tags) {
+        this.tags.clear();
+        for(String tag : tags) {
+            this.tags.add(tag);
+        }
+    }
 }
